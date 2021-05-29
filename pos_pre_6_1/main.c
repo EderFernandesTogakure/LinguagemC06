bcae477c0d0845e6703f63a2bdcc905a22d6450a
Eder Carlos Fernandes <eder.tecnologias@gmail.com>
2021-05-29 17:29:53 -0300

    Add files via upload

---
+++ pos_pre_6_1/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int x1 = 10, x2 = 9;
+    //x1 + 1 = 11
+    x1++;
+    //1 + x2 = 10
+    ++x2;
+
+    printf("O valor de X1 = %d \n", x1);
+    printf("O valor de X2 = %d \n", x2);
+
+}
---
+++ pos_pre_6_2/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x1 = 11, x2 = 13;
+    // x1 - 1 = 10
+    x1--;
+    --x2;
+    printf("O valor de X1 = %d\n", x1);
+    printf("O valor de X2 = %d\n", x2);
+}
